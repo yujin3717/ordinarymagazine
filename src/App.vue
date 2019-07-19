34ae49344c5cf08fda10b737249e596c24e3e769
KTY <kty0529@gmail.com>
2019-07-19 21:55:56 +0900

    가이드 화면 숨김

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="wrap" class="max-container">
     <Spinner v-show="loading"/>
     <!-- <Cover/> -->
-    <Guide/>
+    <!-- <Guide/> -->
     <Header/>
     <Drawer/>
     <Main @setSpinner="setSpin"/>
@@ -35,7 +35,7 @@ export default {
     }
   },
   mounted(){
-    
+
   },
   methods:{
     setSpin: function() {
@@ -49,7 +49,7 @@ export default {
     }
   }
 
-  
+
 }
 </script>
 
